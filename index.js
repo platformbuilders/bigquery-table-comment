7ae544d1be85ece9202ac2e417c1c89593f67782
Rafael Karczevski <rafael.karczevski@platformbuilders.io>
2024-04-17 11:09:22 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const { BigQuery } = require('@google-cloud/bigquery');
+const bigquery = new BigQuery({
+    projectId: core.getInput('project-id'),
+});
+let tableResponse = '';
+
+const query = core.getInput('query');
+
+bigquery.createQueryStream(query)
+    .on('error', function (err) {
+        core.setFailed(err);
+    })
+    .on('data', function (row) {
+        if (tableResponse === '') {
+            Object.keys(row).forEach((key, index) => {
+                tableResponse += `| ${key} `;
+            });
+            tableResponse += ` | 
+`
+            Object.keys(row).forEach((key, index) => {
+                tableResponse += "| --- ";
+            });
+            tableResponse += ` | 
+`
+        }
+        Object.keys(row).forEach((key, index) => {
+            tableResponse += `| ${row[key]} `;
+        });
+        tableResponse += ` | 
+`
+    })
+    .on('end', function () {
+        core.setOutput("table", tableResponse);
+    });
+
+bigquery.createQueryStream(query)
+    .on('data', function (row) {
+        this.end();
+    });
